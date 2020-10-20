840742d3245596620bcc648b07b2c8e1f3c11dc6
ilivss <ilivss@pm.me>
2020-10-20 14:50:00 +0200

    Tiles  toegevoegd

---
+++ src/main/java/perio/tiles/DecorationTile.java
@@ -0,0 +1,13 @@
+package perio.tiles;
+
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.tile.Tile;
+
+public class DecorationTile extends Tile {
+    /**
+     * @param sprite The image which will be drawn whenever the draw method of the tile is called.
+     */
+    public DecorationTile(Sprite sprite) {
+        super(sprite);
+    }
+}
